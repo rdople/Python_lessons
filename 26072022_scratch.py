c2f4df68f7d297e6fe2a28d602112d615800aa42
rdople <vitaliyradchenko1@gmail.com>
2022-07-26 21:32:25 +0300

    scratch

--- 26072022_scratch.py
+++ 26072022_scratch.py
@@ -1,3 +1,14 @@
 # sum of two numbers
-result =1+1 # there is the sum of two numbers
-print ("Result ",result)
+# result =1+1 # there is the sum of two numbers
+# print ("Result ",result)
+
+# value_str = "some string"
+# value_int = 13
+# result = value_int + value_str
+# print(result)
+
+new_var = 13
+print(new_var, type(new_var))
+
+new_var = "some string"
+print(new_var, type(new_var))
