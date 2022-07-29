315e3982e4992ba1c828e2e5c89ed095d6cbe3ad
rdople <vitaliyradchenko1@gmail.com>
2022-07-29 14:35:19 +0300

    homework data_types

---
+++ HW2/data_types.py
@@ -0,0 +1,31 @@
+# Практичне завдання
+#     Створи файл data_types.py у своєму проекті, виконуй завдання в ньому, використовувати англійську
+#     Після виконання практичної роботи - запуш її у GitHub
+#     Створи 10 змінних, дотримуючись правил іменування у Python.
+#     У якості даних використовуй типи даних int та str
+#     За допомогою print() роздрукуй змінні та їхній тип даних
+
+first_name = "Jack"
+last_name = "Brown"
+nick_name = "JB"
+country = "USA"
+city = "Detroit"
+zip_code = 3216789
+user_id = 123456
+age = 33
+weight_kg = 60
+height_cm = 170
+
+print("User info:")
+print("First name:", first_name, type(first_name))
+print(" Last name:", last_name, type(last_name), "\n")
+print("Location:")
+print("   Country:", country, type(country))
+print("      City:", city, type(city))
+print("  ZIP-code:", zip_code, type(zip_code), "\n")
+print("Other:")
+print(" Nick name:", nick_name, type(nick_name))
+print("        ID:", user_id, type(user_id))
+print("       Age:", age, " y.o.", type(age), )
+print("    Weigth:", weight_kg, " kg", type(weight_kg))
+print("    Height:", height_cm, "cm", type(height_cm))
