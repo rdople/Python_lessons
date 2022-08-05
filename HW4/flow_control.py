212d7bcd0f8b569bcc6edc7413437a5299af9ea2
rdople <vitaliyradchenko1@gmail.com>
2022-08-05 15:34:44 +0300

    Dividing on zero and check bad action symbol Homework flow_control

--- HW4/flow_control.py
+++ HW4/flow_control.py
@@ -16,14 +16,8 @@ print("Now you're in calculator mode, enter two operands and basic actions")
 num_a = input("Enter your first number: ")
 num_b = input("Enter your second number: ")
 result = None
-action = input(f"Enter what operation you want to do with your two numbers"
+action = input(f"Enter operation symbol:"
                f"\n P.S. Only * / + - ** operations expected: ")
-if action != "*" or "-" or "+" or "/" or "**":
-    print(f"You've entered incorrect action symbol. \n"
-          f"We can't calculate it. \n"
-          f" Your result wil be None ")
-#    action = input(f"You've entered incorrect action symbol. \n"
-#                   f"Try again. Only * / + - ** operations expected: ")
 num_a = float(num_a) if num_a.find(".") != -1 else int(num_a)
 num_b = float(num_b) if num_b.find(".") != -1 else int(num_b)
 match action:
@@ -34,10 +28,11 @@ match action:
     case "*":
         result = num_a * num_b
     case "/":
-        result = num_a / num_b
+        result = num_a / num_b if num_b != 0 else "Can't divide on zero!"
     case "**":
         result = num_a ** num_b
-
+    case _:
+        print("Bad symbol. Only * / + - ** operations expected")
 print(f"Your result: {result}")
 print("first", num_a, type(num_a))
 print("second", num_b, type(num_b))
