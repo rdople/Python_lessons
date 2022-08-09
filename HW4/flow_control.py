4fd7c954697bbfeeca657f9f37d5b2002e783f10
rdople <vitaliyradchenko1@gmail.com>
2022-08-09 17:16:15 +0300

    added loop when in input

--- HW4/flow_control.py
+++ HW4/flow_control.py
@@ -11,30 +11,46 @@
 #  Тип операнду
 #  Скільки порядків має операнд
 #  Результат порівняння операндів
-
-print("Now you're in calculator mode, enter two operands and basic actions")
-num_a = input("Enter your first number: ")
-num_b = input("Enter your second number: ")
+num_a = 0
+num_b = 0
+action = ""
 result = None
-action = input(f"Enter operation symbol:"
-               f"\n P.S. Only * / + - ** operations expected: ")
-num_a = float(num_a) if num_a.find(".") != -1 else int(num_a)
-num_b = float(num_b) if num_b.find(".") != -1 else int(num_b)
-match action:
-    case "+":
-        result = num_a + num_b
-    case "-":
-        result = num_a - num_b
-    case "*":
-        result = num_a * num_b
-    case "/":
-        result = num_a / num_b if num_b != 0 else "Can't divide on zero!"
-    case "**":
-        result = num_a ** num_b
-    case _:
-        print("Bad symbol. Only * / + - ** operations expected")
-print(f"Your result: {result}")
-print("first", num_a, type(num_a))
-print("second", num_b, type(num_b))
+quit_word = "exit"
+while True:
+    print("Now you're in calculator mode, enter two operands and basic actions")
+    num_a = input("Enter your first number: ")
+    if num_a == quit_word:
+        break
+    else:
+        num_b = input("Enter your second number: ")
+    if num_b == quit_word:
+        break
+    else:
+        action = input(f"Enter operation symbol:"
+                       f"\n P.S. Only * / + - ** operations expected: ")
+        if action == quit_word:
+            break
+        else:
+            num_a = float(num_a) if num_a.find(".") != -1 else int(num_a)
+            num_b = float(num_b) if num_b.find(".") != -1 else int(num_b)
+            match action:
+                case "+":
+                    result = num_a + num_b
+                case "-":
+                    result = num_a - num_b
+                case "*":
+                    result = num_a * num_b
+                case "/":
+                    result = num_a / num_b if num_b != 0 else "Can't divide on zero!"
+                case "**":
+                    result = num_a ** num_b
+                case _:
+                    print("Bad symbol. Only * / + - ** operations expected")
+            print(f"Your result: {result}")
+            print("first", num_a, type(num_a))
+            print("second", num_b, type(num_b))
+
+
+
 
 
