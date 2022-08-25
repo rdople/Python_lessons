8a00f09640088841ddfd6d57d62bca692e138235
rdople <vitaliyradchenko1@gmail.com>
2022-08-25 17:21:23 +0300

    Створи функцію, яка приймає параметри start та end, підсумовує всі цілі числа від значення start до величини end включно. Якщо користувач задасть перше число більше, ніж друге, поміняй їх місцями
    Створи функцію, яка відображає число секунд у вигляді дні:години:хвилини:секунди. Функція може прийняти число як рядок так і як ціле число.
    
    Створи три функції, які обчислюють суму чисел у списку
    
        з for-циклом
    
        з while-циклом

--- HW8/functions.py
+++ HW8/functions.py
@@ -2,59 +2,80 @@
 # підсумовує всі цілі числа від значення start до величини end включно.
 # Якщо користувач задасть перше число більше, ніж друге, поміняй їх місцями
 
-# while True:
-#     try:
-#         number_1 = int(input("Enter first number:"))
-#
-#     except ValueError as error:
-#         print("Enter only digits")
-#     try:
-#         number_2 = int(input("Enter second number:"))
-#         break
-#     except ValueError as error:
-#         print("Enter only digits")
-#
-#
-# def int_sum(start, end):
-#     if start > end:
-#         start, end = end, start
-#     return sum([el for el in range(start, end + 1)])
-#
-#
-# print(int_sum(number_1, number_2))
+while True:
+    try:
+        number_1 = int(input("Enter first number:"))
+
+    except ValueError as error:
+        print("Enter only digits")
+    try:
+        number_2 = int(input("Enter second number:"))
+        break
+    except ValueError as error:
+        print("Enter only digits")
+
+
+def int_sum(start, end):
+    if start > end:
+        start, end = end, start
+    return sum([el for el in range(start, end + 1)])
+
+
+print(int_sum(number_1, number_2))
 
 # Створи функцію, яка відображає число секунд у вигляді дні:години:хвилини:секунди.
 # Функція може прийняти число як рядок так і як ціле число.
 
 
-# def time_representation(sec):
-#     days = sec // (24 * 3600)
-#     sec = sec % (24 * 3600)
-#     hours = sec // 3600
-#     sec %= 3600
-#     minutes = sec // 60
-#     sec %= 60
-#     print(f"дні:години:хвилини:секунди")
-#     print(days, hours, minutes, sec, sep=':')
-#     return None
-#
-#
-# while True:
-#     try:
-#         user_input_seconds = int(input("Enter total seconds: "))
-#         break
-#     except ValueError as error:
-#         print("Enter only digits")
-#
-# time_representation(user_input_seconds)
+def time_representation(sec):
+    days = sec // (24 * 3600)
+    sec = sec % (24 * 3600)
+    hours = sec // 3600
+    sec %= 3600
+    minutes = sec // 60
+    sec %= 60
+    print(f"дні:години:хвилини:секунди")
+    return print(days, hours, minutes, sec, sep=':')
 
 
-# Створи три функції, які обчислюють суму чисел у списку
-#     з for-циклом
+while True:
+    try:
+        user_input_seconds = int(input("Enter total seconds: "))
+        break
+    except ValueError as error:
+        print("Enter only digits")
+
+time_representation(user_input_seconds)
 
 
+# Створи три функції, які обчислюють суму чисел у списку cycles_list
+# #     з for-циклом
+
+def for_loop_list():
+    loop_list = list(input("Enter some sequence of numbers(for):"))
+    print(f"{loop_list=}", type(loop_list))
+    sum_for = 0
+    for x in loop_list:
+        sum_for += int(x)
+    print(f"{sum_for=}")
+    return None
 
 
-
-# Створи три функції, які обчислюють суму чисел у списку
 #     з while-циклом
+
+
+def while_loop_list():
+    loop_list = list(input("Enter some sequence of numbers(while):"))
+    print(f"{loop_list=}", type(loop_list))
+    counter = 0
+    sum_while = 0
+    while counter < len(loop_list):
+        sum_while += int(loop_list[counter])
+        counter += 1
+
+    print(f"{sum_while=}")
+    return None
+
+
+for_loop_list()
+while_loop_list()
