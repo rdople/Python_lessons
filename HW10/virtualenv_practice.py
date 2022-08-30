fbe5694bad45c0cff95444d3d770bc6fc26b00c6
rdople <vitaliyradchenko1@gmail.com>
2022-08-30 18:04:36 +0300

    #    Створи файл virtual_env.py у своєму проекті, виконуй завдання в ньому, #    використовуй англійську # #     Після виконання практичної роботи - запуш її у GitHub # #     Використовуючи термінал зроби наступне: # #         Створи та активуй віртуальне оточення # #         Встанови кілька будь-яких модулів з переліку вище у розділі # #         Збережи перелік залежностей проекту у requirements.txt # #         Деактивуй віртуальне оточення # #     У virtualenv_practice.py у вигляді коментарів запиши команди по порядку, # які ти використав/ла для того щоб досягти результату

---
+++ HW10/virtualenv_practice.py
@@ -0,0 +1,14 @@
+# відкрили  CMD (Win + R) -> написали cmd  ->  OK  -> відкрилося вікно терміналу
+# Створюємо нове оточення, вказуючи шлях та назву нового оточення
+# D:\>python -m venv D:\Study\Hillel\env\my_new_env
+# Активуємо створене оточення
+# D:\Study\Hillel\env\my_new_env\Scripts\activate.bat
+# Встановлюємо перелік модулів:
+# pip install python-telegram-bot
+# pip install SQLAlchemy
+# pip install tabulate
+# pip install requests
+# Зберігаємо список модулів(залежностей) в файлі requirements.txt
+# pip freeze > D:\Study\Hillel\Projects\first_lesson\HW10\requirements.txt
+# деактивуємо оточення
+# deactivate
