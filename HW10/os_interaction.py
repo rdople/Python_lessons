064a4f1dc62517b67a811bfe5e33b2f10452268a
rdople <vitaliyradchenko1@gmail.com>
2022-08-30 14:26:37 +0300

    # Створи файл os_interaction.py у своєму проекті, виконуй завдання в ньому, використовуй англійську # Після виконання практичної роботи - запуш її у GitHub # Зайди на сторінку офіційної документації, ознайомся с функціоналом модуля # Створи функцію яка збирає усю можливу інформацію про операційну систему та записує її у файл os_report.json

---
+++ HW10/os_interaction.py
@@ -0,0 +1,23 @@
+# Створи файл os_interaction.py у своєму проекті, виконуй завдання в ньому, використовуй англійську
+# Після виконання практичної роботи - запуш її у GitHub
+# Зайди на сторінку офіційної документації, ознайомся с функціоналом модуля
+# Створи функцію яка збирає усю можливу інформацію про операційну систему та записує її у файл os_report.json
+
+import os
+import json
+
+obj = {
+    "os": os.environ["OS"],
+    "computer_name": os.environ["COMPUTERNAME"],
+    "user_name": os.environ["USERNAME"],
+    "system_drive": os.environ["SYSTEMDRIVE"],
+    "system_root": os.environ["SYSTEMROOT"],
+    "number_of_processors": os.environ["NUMBER_OF_PROCESSORS"],
+    "processor_identifier": os.environ["PROCESSOR_IDENTIFIER"],
+    "processor_revision": os.environ["PROCESSOR_REVISION"]
+}
+
+with open("os_report.json", "w") as fh:
+    json.dump(obj, fh)
+print(os.environ)
+os.startfile("os_report.json")
